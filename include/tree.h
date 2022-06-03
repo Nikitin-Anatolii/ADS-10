a77f018b305153c6bbe1e8d98c21f79e06dcbec3
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-06-03 15:12:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,70 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <string>
+
+class tree {
+ private:
+  struct Node {
+    char value;
+    std::vector<Node*> bran;
+  };
+  Node *root;
+  const int size = 1000;
+  std::string pointers[size] = { "" };
+  void make(Node *root, std::vector<char> vec) {
+    if (vec.size()) {
+      if (root->value != 'x') {
+        for (auto i = vec.begin(); i != vec.end(); ++i) { 
+          if (*i == root->value) {
+            vec.erase(i);
+            break;
+          }
+        }
+      }
+      for (int i = 0; i < vec.size(); ++i) {
+        root->bran.push_back(new Node);
+      }
+      for (int i = 0; i < root->bran.size(); ++i) {
+        root->bran[i]->value = vec[i];
+      }
+      for (int i = 0; i < root->bran.size(); ++i) {
+        make(root->bran[i], vec);
+      }
+    }
+    return;
+  }
+  void permutate(Node *root, std::string w) {
+  if (!root->bran.size()) {
+    w += root->value;
+    int n = 0;
+    while (pointers[n] != "")
+      ++n;
+    pointers[n] = w;
+    return;
+  }
+  if (root->value != '*') {
+    w += root->value;
+  }
+  for (int i = 0; i < root->bran.size(); ++i) {
+    permutate(root->bran[i], w);
+  }
+}
+  
+ public:
+  std::string permutt(int i) const {
+    if (i >= pointers.size()) {
+      return "";
+     else
+       return pointers[i - 1];
+  }
+  explicit tree(std::vector<char> vec) {
+    root = new Node;
+    root -> value = '*';
+    make(root, vec);
+    permutate(root, "");
+  }
+};
 
 #endif  // INCLUDE_TREE_H_
